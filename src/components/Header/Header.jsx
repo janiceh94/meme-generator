35517a89673814460ef93a75ed4f4c1d09216c15
janiceh94 <haesung94@gmail.com>
2022-10-06 23:01:50 -0700

    styling to header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Header.css';
 import Troll from '../../images/Troll Face.png'
 
 const Header = () => {
     return (
         <header className = 'header'>
-            <img src = {Troll} alt = "logo"/>
-            <h1>Header</h1>
+            <img className = 'header-image' src = {Troll} alt = "logo"/>
+            <h1 className = 'header-title'>Header</h1>
         </header> 
     )
 }
