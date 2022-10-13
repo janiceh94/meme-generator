4f02dafd1c7d69d7dc8c468f0522d16c78d77438
janiceh94 <haesung94@gmail.com>
2022-10-13 12:54:36 -0700

    added meme data

--- src/components/Meme/Meme.jsx
+++ src/components/Meme/Meme.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import './Meme.css';
+import memeData from '../../memeData';
 
 const Meme = () => {
     return (
         <main className='meme-main'>
-            <form className='form'>
+            <div className='form'>
                 <input type="text" className="form-input"
                 placeholder="Top Text"/>
                 <input type="text" className="form-input"
                 placeholder="Bottom Text"
                 />
                 <button className='form-btn'> Get a new meme image</button>
-            </form>
+            </div>
         </main>
     )
 }
