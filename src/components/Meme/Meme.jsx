c2dfb30777c2312c736432ebc66d4b910e0586b3
janiceh94 <haesung94@gmail.com>
2022-10-13 12:03:35 -0700

    created meme component

---
+++ src/components/Meme/Meme.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Meme.css';
+
+const Meme = () => {
+    return (
+        <main>
+            <form>
+                <input type="text"/>
+                <input type="text"/>
+                <button> Get a new meme image</button>
+            </form>
+        </main>
+    )
+}
+
+export default Meme
